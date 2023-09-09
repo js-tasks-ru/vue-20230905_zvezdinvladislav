fdccc299acf2a6b94d965e0c0d505d4a533676c2
vlad_z <zvezdinvladislav@yandex.ru>
2023-09-09 08:52:05 +0300

    01-basics/02-VueCalc

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,37 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp, defineComponent } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+const App = defineComponent({
+  name: 'App',
+  data() {
+    return {
+      firstArg: null,
+      secondArg: null,
+      action: null,
+    };
+  },
+
+  computed: {
+    result() {
+      switch (this.action) {
+        case 'sum':
+          return this.firstArg + this.secondArg;
+          break;
+        case 'subtract':
+          return this.firstArg - this.secondArg;
+          break;
+        case 'multiply':
+          return this.firstArg * this.secondArg;
+          break;
+        case 'divide':
+          return this.firstArg / this.secondArg;
+          break;
+      }
+    },
+  },
+});
+
+const app = createApp(App);
+const vm = app.mount('#app');
+
+window.vm = vm;

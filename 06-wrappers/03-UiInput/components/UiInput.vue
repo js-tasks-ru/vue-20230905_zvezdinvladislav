0371bb70454834ecf4e6072fe329d89eba7cf959
vlad_z <zvezdinvladislav@yandex.ru>
2023-10-06 14:19:39 +0300

    06/03 add solution

--- 06-wrappers/03-UiInput/components/UiInput.vue
+++ 06-wrappers/03-UiInput/components/UiInput.vue
@@ -1,13 +1,30 @@
 <template>
-  <div class="input-group input-group_icon input-group_icon-left input-group_icon-right">
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+  <div
+    class="input-group"
+    :class="[
+      {
+        'input-group_icon': hasIcon(),
+        'input-group_icon-left': hasLeftIcon(),
+        'input-group_icon-right': hasRightIcon(),
+      },
+      $attrs.class,
+    ]"
+  >
+    <div v-if="hasLeftIcon()" class="input-group__icon">
+      <slot name="left-icon" />
     </div>
 
-    <input ref="input" class="form-control form-control_rounded form-control_sm" />
+    <component
+      :is="tag"
+      ref="input"
+      class="form-control"
+      v-bind="{ ...$attrs, class: inputClasses }"
+      :value="modelValue"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
 
-    <div class="input-group__icon">
-      <img class="icon" alt="icon" />
+    <div v-if="hasRightIcon()" class="input-group__icon">
+      <slot name="right-icon" />
     </div>
   </div>
 </template>
@@ -15,6 +32,54 @@
 <script>
 export default {
   name: 'UiInput',
+
+  inheritAttrs: false,
+
+  props: {
+    modelValue: String,
+
+    small: {
+      type: Boolean,
+    },
+
+    rounded: {
+      type: Boolean,
+    },
+
+    multiline: {
+      type: Boolean,
+    },
+  },
+
+  computed: {
+    tag() {
+      return this.multiline ? 'textarea' : 'input';
+    },
+
+    inputClasses() {
+      return { 'form-control_rounded': this.rounded, 'form-control_sm': this.small };
+    },
+  },
+
+  emits: ['update:modelValue'],
+
+  methods: {
+    focus() {
+      this.$refs.input.focus();
+    },
+
+    hasLeftIcon() {
+      return !!this.$slots['left-icon'];
+    },
+
+    hasRightIcon() {
+      return !!this.$slots['right-icon'];
+    },
+
+    hasIcon() {
+      return this.hasLeftIcon() || this.hasRightIcon();
+    },
+  },
 };
 </script>
 

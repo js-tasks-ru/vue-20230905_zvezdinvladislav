c89082e44c76dbc0adee146557b7882df13b73bd
Vlad-95 <zvezdinvladislav@yandex.ru>
2023-10-21 17:15:15 +0500

    09/03 wip

--- 09-rendering/03-UiPanes-JSX/components/UiPanes.vue
+++ 09-rendering/03-UiPanes-JSX/components/UiPanes.vue
@@ -1,19 +1,19 @@
-<template>
-  <div class="panes">
-    <!--  Pane 0 -->
-    <div class="pane">
-      <div class="pane__content">
-        <!-- Определяем, какой именно слот рендерить по массиву порядка панелей -->
-        <slot :name="`pane-${panes[0]}`" />
-      </div>
-      <div class="pane__controls">
-        <!-- Скрываем кнопку классом pane__disabled-button -->
-        <UiButton class="pane__disabled-button" variant="secondary" block @click="up(0)"> Up </UiButton>
+<!-- <template>
+  <div class="panes"> -->
+<!--  Pane 0 -->
+<!-- <div class="pane">
+      <div class="pane__content"> -->
+<!-- Определяем, какой именно слот рендерить по массиву порядка панелей -->
+<!-- <slot :name="`pane-${panes[0]}`" /> -->
+<!-- </div>
+      <div class="pane__controls"> -->
+<!-- Скрываем кнопку классом pane__disabled-button -->
+<!-- <UiButton class="pane__disabled-button" variant="secondary" block @click="up(0)"> Up </UiButton>
         <UiButton variant="danger" block @click="down(0)"> Down </UiButton>
       </div>
-    </div>
-    <!--  Pane 1 -->
-    <div class="pane">
+    </div> -->
+<!--  Pane 1 -->
+<!-- <div class="pane">
       <div class="pane__content">
         <slot :name="`pane-${panes[1]}`" />
       </div>
@@ -21,9 +21,9 @@
         <UiButton variant="secondary" block @click="up(1)"> Up </UiButton>
         <UiButton variant="danger" block @click="down(1)"> Down </UiButton>
       </div>
-    </div>
-    <!--  Pane 2 -->
-    <div class="pane">
+    </div> -->
+<!--  Pane 2 -->
+<!-- <div class="pane">
       <div class="pane__content">
         <slot :name="`pane-${panes[2]}`" />
       </div>
@@ -33,7 +33,7 @@
       </div>
     </div>
   </div>
-</template>
+</template> -->
 
 <script lang="jsx">
 // Предлагается решать задачу с использованием JSX, но вы можете использовать и чистые рендер-функции
@@ -54,7 +54,7 @@ export default {
        * [0, 1, 2]
        * @type {number[]|null}
        */
-      panes: [0, 1, 2],
+      panes: [0, 1, 2, 3],
       // Сейчас здесь массив ровно из трёх элементов, но решение должно быть универсальным для любого количества узлов
     };
   },
@@ -81,6 +81,59 @@ export default {
       this.panes[i] = this.panes[i + 1];
       this.panes[i + 1] = temp;
     },
+  },
+
+  computed: {
+    slotsArr() {
+      return Object.entries(this.$slots).map(([name, func]) => ({
+        name,
+        func,
+      }));
+    },
+  },
+
+  render() {
+    return (
+      <div class="panes">
+        {this.slotsArr.map((item, index) => {
+          const slotName = `pane-${this.panes[index]}`;
+          {
+            /* const slotsArr = Object.entries(this.$slots);
+          console.log(slotsArr); */
+          }
+          return (
+            <div class="pane" key={index}>
+              <div class="pane__content">{item.func()}</div>
+              <div class="pane__controls">
+                <UiButton
+                  class={index == 0 ? 'pane__disabled-button' : ''}
+                  variant="secondary"
+                  block
+                  onClick={() => {
+                    this.up(index);
+                  }}
+                >
+                  {' '}
+                  Up{' '}
+                </UiButton>
+                <UiButton
+                  class={index == this.panes.length - 1 ? 'pane__disabled-button' : ''}
+                  variant="danger"
+                  block
+                  onClick={() => {
+                    console.log();
+                    this.down(index);
+                  }}
+                >
+                  {' '}
+                  Down{' '}
+                </UiButton>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    );
   },
 };
 </script>

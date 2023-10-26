433454dd41062337b24f7b5deab0cbdbf0359b66
vlad_z <zvezdinvladislav@yandex.ru>
2023-10-26 20:56:58 +0300

    12/01 wip

--- 12-provide-inject/01-UiButtonGroup-provide/components/UiButtonGroupItem.vue
+++ 12-provide-inject/01-UiButtonGroup-provide/components/UiButtonGroupItem.vue
@@ -1,5 +1,13 @@
 <template>
-  <button class="button-group__button button-group__button_active" type="button" aria-selected="false">Button</button>
+  <button
+    class="button-group__button"
+    :class="{ 'button-group__button_active': isActive }"
+    type="button"
+    aria-selected="false"
+    @click="setModelValue(value)"
+  >
+    <slot />
+  </button>
 </template>
 
 <script>
@@ -9,6 +17,14 @@ export default {
   props: {
     value: {
       required: true,
+    },
+  },
+
+  inject: ['modelValue', 'setModelValue'],
+
+  computed: {
+    isActive() {
+      return this.value === this.modelValue;
     },
   },
 };
